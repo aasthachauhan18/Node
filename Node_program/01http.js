1dceeec6a4cd986b0a065645338dc0daa3b674bd
Aastha1810-stack <chauhanaastha1810@gmail.com>
2026-01-27 22:49:17 +0530

    1/27/2026

--- Node_program/01http.js
+++ Node_program/01http.js
@@ -1,5 +1,8 @@
 // let http = require("http");
-import http from "http"
+// import http from "http"
+const http = require('http');
+const fs = require('fs')
+const path = require('path');
 // const server = http.createServer((req,res) =>{
 //     console.log("URL: ",req.url);
 //     console.log("Method :" ,req.method);
@@ -17,24 +20,46 @@ http.createServer((req,res) =>{
     console.log("Headers:",req.headers);
     
     if (req.url === "/") {
-        res.end("Home page");
+        const filePath = path.join(__dirname,'pages','Home.html');
+
+        fs.readFile(filePath, (err,data) => {
+            if(err){
+                res.writeHead(500, {'content-type' : 'text/html'});
+                return res.end('<h1>500 - server error');
+            }
+            res.writeHead(200 , {'content-type' : 'text/html'});
+            res.end(data);
+        });
+
+        // res.end("Home page");
     } else if (req.url==="/about") {
-        res.end("<h1>Hello</h1>")
+         const filePath = path.join(__dirname,'pages','About.html');
+
+        fs.readFile(filePath, (err,data) => {
+            if(err){
+                res.writeHead(500, {'content-type' : 'text/html'});
+                return res.end('<h1>500 - server error');
+            }
+            res.writeHead(200 , {'content-type' : 'text/html'});
+            res.end(data);
+        });
+        // res.end("<h1>Hello</h1>")
         // res.end("test")
-    } else if (req.url === "/json") {
-        const data = {
-            name:"Node Server",
-            status:"Running"
-        };
-        res.setHeader("Content-type","applocation/json");
-        res.end(JSON.stringify(data));
-    }
+    } 
+    // else if (req.url === "/json") {
+    //     const data = {
+    //         name:"Node Server",
+    //         status:"Running"
+    //     };
+    //     res.setHeader("Content-type","applocation/json");
+    //     res.end(JSON.stringify(data));
+    // }
     else{
         res.statusCode = 404;
         res.end("Page not found");
     }
-    res.end("Request logged");
-    res.end("This is the example of node.js web-based application\n");
+    // res.end("Request logged");
+    // res.end("This is the example of node.js web-based application\n");
 }).listen(5000, () => console.log("server started at localhost:5000"))
 
 
